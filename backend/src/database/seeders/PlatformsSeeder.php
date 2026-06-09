2252804005fd82f3ab532d2ba9ac959d5f9b5dbe
Mawkli <aymanemj@proton.me>
2026-06-09 11:57:47 +0100

    Seeders for MetricKeys & Platforms

---
+++ backend/src/database/seeders/PlatformsSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PlatformsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $platforms = [
+            [
+                'platform_name' => 'web4jobs_progress',
+                'weight'        => 30,
+                'key'           => 'web4jobs_progress',
+            ],
+
+        ];
+
+        foreach ($platforms as $platform) {
+            DB::table('platforms')->insert([
+                ...$platform,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
