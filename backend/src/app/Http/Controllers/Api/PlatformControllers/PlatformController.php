39b972d2454f57a08f217fef2fd38c47cd4a3ecc
Mawkli <aymanemj@proton.me>
2026-06-10 11:04:35 +0100

    Controller for each platform for scalability
        -AttendanceCenterEventController
        -CertificationPlatformEventController
        -DiscordEventController
        -InsertionPlatformEventController
        -ManualContributionEventController
        -PlatformController
        -Web4JobsPlatformEventController

---
+++ backend/src/app/Http/Controllers/Api/PlatformControllers/PlatformController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\PlatformControllers;
+
+use Exception;
+use Illuminate\Http\Request;
+use App\Services\PlatformService;
+use App\Http\Requests\Web4JobsEventRequest;
+
+class PlatformController extends Controller
+{
+    public function __construct(private PlatformService $platformService) {}
+
+    public function managePlatformRequest(Web4JobsEventRequest $request)
+    {
+        try {
+            return $this->platformService->managePlatformRequest($request);
+        } catch (Exception $exception) {
+        }
+    }
+}
