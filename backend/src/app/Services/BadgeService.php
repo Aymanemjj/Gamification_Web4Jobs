a38a66c23c9da1ecb99cd32618895fd9dbae99b5
Mawkli <aymanemj@proton.me>
2026-06-10 10:34:13 +0100

    Basic CRUD for Leagues & Badges

---
+++ backend/src/app/Services/BadgeService.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Resources\BadgeResource;
+use App\Models\Badge;
+use Illuminate\Http\Response;
+use Illuminate\Http\JsonResponse;
+
+class BadgeService
+{
+    public function listAllBadges(): JsonResponse
+    {
+        return response()->json(
+            [
+                "data" => BadgeResource::collection(Badge::all()),
+            ],
+            200,
+        );
+    }
+
+    public function showBadgeDetails(Badge $badge): JsonResponse
+    {
+        return response()->json(
+            [
+                "data" => BadgeResource::make($badge),
+            ],
+            200,
+        );
+    }
+
+    public function createBadge(array $data): JsonResponse
+    {
+        return response()->json(
+            [
+                "data" => BadgeResource::make(Badge::create($data)),
+            ],
+            201,
+        );
+    }
+
+    public function updateBadge(Badge $badge, array $data): JsonResponse
+    {
+        $badge->update($data);
+        return response()->json(
+            [
+                "data" => BadgeResource::make($badge),
+            ],
+            200,
+        );
+    }
+
+    public function deleteBadge(Badge $badge): Response
+    {
+        $badge->delete();
+        return response()->noContent();
+    }
+}
