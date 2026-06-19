9c19a66352f02e216d964c7bdf047aa1880b33e0
Mawkli <aymanemj@proton.me>
2026-06-19 12:07:00 +0100

    EventType Model

--- backend/src/app/Models/EventType.php
+++ backend/src/app/Models/EventType.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class EventType extends Model
 {
-    //
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        "type",
+        "platform_id",
+    ];
+
+    public function platform(): BelongsTo
+    {
+        return $this->belongsTo(Platform::class);
+    }
 }
