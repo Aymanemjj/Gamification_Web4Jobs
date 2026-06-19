244ed420a12df9d4022247c36efe0770107c1d53
Mawkli <aymanemj@proton.me>
2026-06-19 12:06:46 +0100

    EventType DTO

--- backend/src/app/DTOs/EventTypeDTO.php
+++ backend/src/app/DTOs/EventTypeDTO.php
@@ -2,13 +2,34 @@
 
 namespace App\DTOs;
 
-class EventTypeDTO
+use App\Interfaces\BasicDtoInterface;
+use App\Models\Platform;
+
+readonly class EventTypeDTO implements BasicDtoInterface
 {
-    /**
-     * Create a new class instance.
-     */
-    public function __construct()
+    public function __construct(
+        public string $type,
+        public Platform $platform,
+    ) {}
+
+    public static function make(array $data): self
     {
-        //
+        return new self(
+            type: $data['type'],
+            platform: Platform::where('name', $data['platform'])->firstOrFail(),
+        );
+    }
+
+    public static function collection(array $data): array
+    {
+        return array_map(fn($item) => self::make($item), $data);
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'type'        => $this->type,
+            'platform_id' => $this->platform->id,
+        ];
     }
 }
